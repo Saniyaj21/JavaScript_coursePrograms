14e03668748e6bc1a0566e4fa65a924e7e4cbd8d
Saniyaj Mallik <111429200+Saniyaj21@users.noreply.github.com>
2022-12-13 08:20:04 +0530

    Add files via upload

---
+++ Course/V13-Strings.js
@@ -0,0 +1,16 @@
+let myStrings = "This is a string";
+console.log(myStrings.length);
+console.log(myStrings[3]);
+
+// Template Literals  this kind of string formating in python
+let boy1 = 'Saniyaj';
+let boy2 = 'Shubha';
+let greeting = `${boy1} is a friend of ${boy2}`;
+console.log(greeting);
+
+// Escape sequence
+let var2 = 'banana\'s';
+console.log(var2);
+// also we have 
+// \t for a tab space
+// \n for new line
