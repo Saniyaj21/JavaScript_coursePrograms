4d3a06051bb3cb73c2d0e46fc46b82bfd2ea43be
Saniyaj Mallik <111429200+Saniyaj21@users.noreply.github.com>
2022-12-19 13:56:43 +0530

    Add files via upload

--- Course/V11-functions.js
+++ Course/V11-functions.js
@@ -2,7 +2,7 @@ function Sum(a, b) {
     return a + b;
 }
 
-// const Sum2 = (x,y) => {  This is also valid
+// const Sum2 = (x,y) => {  //This is also valid
 const Sum2 = () => {
     return x + y;
 }
@@ -10,8 +10,8 @@ const Sum2 = () => {
 
 
 let x = 5;
-x = Number.parseInt(x);
+
 let y = 52;
-y = Number.parseInt(y);
+
 console.log("Sum of ", x, "and", y, "is =", Sum(x, y));
 console.log("Sum of ", x, "and", y, "is =", Sum2(x, y));
